2b1d2f04b806ca10a9df7e4f20fa325138468b08
SugaryPineapple <sugarypineapple2@gmail.com>
2021-05-11 13:54:25 +0200

    Added unittests to containervervoer and got the first version of game to web connection figured out in the testing enviorment

--- Sprint4/ExtraOpdrachten/DatabaseConntectionTesting/DatabaseConntectionTesting/Program.cs
+++ Sprint4/ExtraOpdrachten/DatabaseConntectionTesting/DatabaseConntectionTesting/Program.cs
@@ -8,21 +8,8 @@ namespace DatabaseConntectionTesting
     {
         static void Main(string[] args)
         {
-            string connectionString = "server=localhost;uid=root;pwd=;database=platformspeedrunner_web_systeem";
-            string dbQuery = "SELECT* FROM platformspeedrunner_web_systeem.run.custom_name;";
-
-            MySqlConnection sqlConnection = new MySqlConnection(connectionString);
-            MySqlCommand sqlCommand = new MySqlCommand(dbQuery, sqlConnection);
-
-            sqlConnection.Open();
-
-            MySqlDataAdapter sqlDataAdapter = new MySqlDataAdapter();
-            sqlDataAdapter.SelectCommand = sqlCommand;
-
-            DataTable dataTable = new DataTable();
-            sqlDataAdapter.Fill(dataTable);
-
-            Console.WriteLine(dataTable);
+            Run run = new Run();
+            run.create(run);
         }
     }
 }
